14d98eed8376d6aa1b2a17699120805fe0b368f0
Guilherme Samuel <guijas.work@gmail.com>
2024-04-18 20:34:03 -0300

    chore: mssql setup

--- batch/setup-airbyte.sh
+++ batch/setup-airbyte.sh
@@ -1,2 +1,8 @@
-cd airbyte/
+# clone Airbyte from GitHub
+git clone --depth=1 https://github.com/airbytehq/airbyte.git
+
+# switch into Airbyte directory
+cd airbyte
+
+# start Airbyte
 ./run-ab-platform.sh
