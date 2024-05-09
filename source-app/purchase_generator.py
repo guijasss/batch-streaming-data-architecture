8358bea276bda20ae9d8bc2c97dd0a48ae4270d4
Guilherme Samuel <guijas.work@gmail.com>
2024-05-09 08:22:17 -0300

    feat: scripts to insert data into tables

--- source-app/purchase_generator.py
+++ source-app/purchase_generator.py
@@ -1,42 +1,88 @@
-from pyodbc import connect
+from pyodbc import connect, Cursor, Connection
 from faker import Faker
 
-from random import randint, uniform
+from numpy import arange
+from random import choice
 from argparse import ArgumentParser
 from time import sleep
 
-# Connection settings
-server = 'localhost'
-database = 'demo'
-username = 'sa'
-password = 'adiosplofe12@@'
-driver = 'ODBC Driver 17 for SQL Server'
 
-url = f'DRIVER={{{driver}}};SERVER={server},1433;DATABASE={database};UID={username};PWD={password}'
+class DatabaseFaker():
+    def __init__(self) -> None:
+        self.server = 'localhost'
+        self.database = 'demo'
+        self.username = 'sa'
+        self.password = 'adiosplofe12@@'
+        self.driver = 'ODBC Driver 17 for SQL Server'
+        self.url = f'DRIVER={{{self.driver}}};SERVER={self.server},1433;DATABASE={self.database};UID={self.username};PWD={self.password}'
+        self.connection: Connection = connect(self.url)
+        self.faker = Faker()
 
-# Establish connection
-conn = connect(url)
-
-# Function to insert records into Sales table
-def insert_into_purchases():
-    cursor = conn.cursor()
-    user_id = randint(1, 4)
-    product_id = randint(1, 5)
-    sql = "INSERT INTO Purchases (user_id, product_id) VALUES (?, ?)"
-    cursor.execute(sql, (user_id, product_id))
-    conn.commit()
-    cursor.close()
-
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("--batch-size", type=int, help="Number of rows to insert in each batch.", required=True)
-    args = parser.parse_args() 
-
-    batch_size: int = args.batch_size
-    fake = Faker()
-
-    for loop in range(batch_size):
-        insert_into_purchases()
+    def get_random_id(self, table: str) -> int:
+        cursor: Cursor = self.connection.cursor()
+        sql = f"SELECT TOP 1 id FROM demo.dbo.{table.title()} ORDER BY NEWID()"
+        random_id: int = cursor.execute(sql).fetchone()[0]
+        cursor.close()
+        return random_id
     
-    print(f"{batch_size} records were inserted!")
-    conn.close()
+    def generate_purchase(self, verbose: bool):
+        cursor: Cursor = self.connection.cursor()
+        user_id: int = self.get_random_id("Users")
+        product_id: int = self.get_random_id("Products")
+        sql = "INSERT INTO demo.dbo.Purchases (user_id, product_id) VALUES (?, ?)"
+        cursor.execute(sql, (user_id, product_id))
+
+        if verbose:
+            print({
+                "user_id": user_id,
+                "product_id": product_id
+            })
+
+        self.connection.commit()
+        cursor.close()
+
+    def generate_rating(self, verbose: bool):
+        cursor: Cursor = self.connection.cursor()
+        purchase_id: int = self.get_random_id("Purchases")
+        purchases_sql_select = f"SELECT user_id, product_id FROM demo.dbo.Purchases WHERE id = {purchase_id}"
+        cursor.execute(purchases_sql_select)
+        
+        user_id, product_id = cursor.fetchone()
+        stars = choice(arange(0, 5.1, 0.5).tolist())
+        comment = self.faker.paragraph()
+        ratings_insert_sql = f"INSERT INTO demo.dbo.Ratings (user_id, product_id, stars, comment) VALUES (?, ?, ?, ?)"
+        cursor.execute(ratings_insert_sql, (user_id, product_id, stars, comment))
+
+        if verbose:
+            print({
+                "user_id": user_id,
+                "product_id": product_id,
+                "stars": stars,
+                "comment": comment
+            })
+
+        self.connection.commit()
+        cursor.close()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--table", help="Table to insert data", type=str)
+    parser.add_argument("--batch-size", help="How many rows to insert in table", type=int)
+    parser.add_argument("--verbose", "-v", help="Whether to print inserted data", const=True, nargs="?")
+    args = parser.parse_args()
+
+    table = args.table
+    batch_size = args.batch_size
+    verbose = args.verbose is not None
+
+    db_faker = DatabaseFaker()
+
+    table_to_insert_function_mapping = {
+        "purchases": db_faker.generate_purchase,
+        "ratings": db_faker.generate_rating
+    }
+
+    for _ in range(batch_size):
+        table_to_insert_function_mapping.get(table)(verbose)
+    
